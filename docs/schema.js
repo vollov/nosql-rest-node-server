32981853038ebc5840451614890991c09bf1c91d
Dustin Zhang <dike.zhang@gmail.com>
2016-01-12 16:28:28 -0500

    updated schema.js

--- docs/schema.js
+++ docs/schema.js
@@ -1,15 +1,21 @@
-users = [{'password': 'passwd', 'is_active': false, 'email': 'mary@demo.org', 'role':1}, 
-         {'password': 'foobar', 'is_active': true, 'email': 'wendy@abc.com', 'role':2}, 
-         {'password': 'passwd', 'is_active': false, 'email': 'dustin@demo.org', 'role':3}, 
-         {'password': 'passwd', 'is_active': true, 'email': 'jenny@demo.org','role':3}, 
-         {'password': 'blah', 'is_active': true, 'email': 'fred@gmail.ca','role':2}];
-roles = [{'name':'admin', 'id':1}, {'name':'landlord', 'id':2}, {'name':'tenant', 'id':3}];
-routes = [{'url':'/home', 'access':'public'}, {'url':'/about', 'access':'public'}];
+db.user.drop();
+
+users = [ {
+	"firstname" : "Thomas",
+	"lastname" : "Davis",
+	"age" : 12
+}, {
+	"firstname" : "Annie",
+	"lastname" : "Chan",
+	"age" : 33
+}, {
+	"firstname" : "Dustin",
+	"lastname" : "Light",
+	"age" : 35
+}, {
+	"firstname" : "Luke",
+	"lastname" : "Smith",
+	"age" : 47
+}, ];
 
 db.user.insert(users);
-db.role.insert(roles);
-db.route.insert(routes);
-
-db.user.ensureIndex({email: 1}, {unique: true});
-db.role.ensureIndex({email: 1}, {unique: true});
-db.route.ensureIndex({url: 1}, {unique: true});
